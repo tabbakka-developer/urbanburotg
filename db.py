7c1e2fb053b10213c16fb65a91566bbc76692320
Vladyslav Popov <vladyslav.popov@inventorsoft.co>
2023-11-27 18:43:57 +0200

    try to fix db

--- db.py
+++ db.py
@@ -2,16 +2,14 @@ import sqlite3
 
 
 class DB:
-    _connection = None
-    _cursor = None
 
     def __int__(self):
-        self._connection = sqlite3.connect('urbanburo_database.db')
-        self._cursor = self._connection.cursor()
+        self.connection = sqlite3.connect('urbanburo_database.db')
+        self.cursor = self.connection.cursor()
         self.init_users_table()
 
     def init_users_table(self):
-        self._cursor.execute('''
+        self.cursor.execute('''
         CREATE IF NOT EXISTS Users (
         id INTEGER PRIMARY KEY
         telegram_id BIGINT
@@ -19,15 +17,15 @@ class DB:
         last_name VARCHAR(255) DEFAULT NULL
         username VARCHAR(255) DEFAULT NULL
         ''')
-        self._connection.commit()
+        self.connection.commit()
 
     def set_user(self, telegram_id, first_name = None, last_name = None, username = None):
-        self._cursor.execute(
+        self.cursor.execute(
             'INSERT INTO Users (telegram_id, first_name, last_name, username) VALUES (?, ?, ?, ?)',
             (telegram_id, first_name, last_name, username)
         )
-        self._connection.commit()
+        self.connection.commit()
 
     def get_user_by_telegram_id(self, telegram_id):
-        self._cursor.execute('SELECT * FROM Users WHERE telegram_id = ?', (telegram_id))
-        return self._cursor.fetchall()
+        self.cursor.execute('SELECT * FROM Users WHERE telegram_id = ?', (telegram_id))
+        return self.cursor.fetchall()
